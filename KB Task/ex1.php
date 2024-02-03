6843dcf58f800185ede2f06a3a8ba145cb432c30
Kapila Bandara <126715560+kapilakbandara@users.noreply.github.com>
2024-02-03 15:54:04 +0200

    ex2 done

--- KB Task/ex1.php
+++ KB Task/ex1.php
@@ -48,7 +48,7 @@
             <td><?php echo $g3 ?></td>
         </tr> 
     </table>
-    <h3>3.5 Take a screenshot that confirms your development environment 
+    <h3>3.4 Take a screenshot that confirms your development environment 
         setup and include it as an image in the "ex1.php" file.</h3>
     <img src="images/2024.png" alt="web development environment">
     </body>
---
+++ KB Task/footer.php
@@ -0,0 +1,11 @@
+<footer class="row mt-5" style="text-align: center;">
+            <div class="col-sm-4">@Kapila Bandara</div>
+            <div class="col-sm-4">Course: Web Development</div>
+            <div class="col-sm-4 text-center">HAMK</div>
+        </footer>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script         
+
+       
+</body>
+</html>
